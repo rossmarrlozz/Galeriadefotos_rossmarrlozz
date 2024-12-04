1715a93a905e25f342dc361e25998bf6a6037438
Mariana Morales <168719962+rossmarrlozz@users.noreply.github.com>
2024-12-04 10:28:43 +0000

    test

--- src/app.js
+++ src/app.js
@@ -3,21 +3,13 @@ import "bootstrap";
 import "./style.css";
 
 function upDate(previewPic) {
-  /* 1) change the url for the background image of the div with the id = "image" 
-       to the source file of the preview image */
   document.getElementById("image").style.backgroundImage =
     "url(" + previewPic.src + ")";
-  /* 2) Change the text of the div with the id = "image" 
-       to the alt text of the preview image */
   document.getElementById("image").innerHTML = previewPic.alt;
 }
 
 function unDo() {
-  /* 1) Update the url for the background image of the div with the id = "image" 
-       back to the original image. You can use the CSS code to see what that original URL was */
   document.getElementById("image").style.backgroundImage = "none";
-  /* 2) Change the text of the div with the id = "image" 
-       back to the original text. You can use the HTML code to see what that original text was */
   document.getElementById("image").innerHTML =
     "Hover over an image below to display here.";
 }
